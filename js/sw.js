28512265d634a0c7a9794d0ac8aa46825f848dc9
Francisco Ferioli Marco <franciscoferiolimarco@protonmail.com>
2023-02-11 04:40:33 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,53 +1,80 @@
-self.importScripts('data/games.js');
-
-// Files to cache
-const cacheName = 'js13kPWA-v1';
-const appShellFiles = [
-  '/pwa/',
-  '/pwa/index.html',
-  '/pwa/js/app.js',
-  '/pwa/css/style.css',
-  '/pwa/fonts/graduate.eot',
-  '/pwa/fonts/graduate.ttf',
-  '/pwa/fonts/graduate.woff',
-  '/pwa/favicon.ico',
-  '/pwa/img/js13kgames.png',
-  '/pwa/img/bg.png',
-  '/pwa/icons/icon-32.png',
-  '/pwa/icons/icon-64.png',
-  '/pwa/icons/icon-96.png',
-  '/pwa/icons/icon-128.png',
-  '/pwa/icons/icon-168.png',
-  '/pwa/icons/icon-192.png',
-  '/pwa/icons/icon-256.png',
-  '/pwa/icons/icon-512.png',
-];
-const gamesImages = [];
+// Generating content based on the template
+const template = `<article>
+  <img src='data/img/placeholder.png' data-src='data/img/SLUG.jpg' alt='NAME'>
+  <h3>#POS. NAME</h3>
+  <ul>
+  <li><span>Author:</span> <strong>AUTHOR</strong></li>
+  <li><span>Twitter:</span> <a href='https://twitter.com/TWITTER'>@TWITTER</a></li>
+  <li><span>Website:</span> <a href='http://WEBSITE/'>WEBSITE</a></li>
+  <li><span>GitHub:</span> <a href='https://GITHUB'>GITHUB</a></li>
+  <li><span>More:</span> <a href='http://js13kgames.com/entries/SLUG'>js13kgames.com/entries/SLUG</a></li>
+  </ul>
+</article>`;
+let content = '';
 for (let i = 0; i < games.length; i++) {
-  gamesImages.push(`data/img/${games[i].slug}.jpg`);
+  let entry = template.replace(/POS/g, (i + 1))
+    .replace(/SLUG/g, games[i].slug)
+    .replace(/NAME/g, games[i].name)
+    .replace(/AUTHOR/g, games[i].author)
+    .replace(/TWITTER/g, games[i].twitter)
+    .replace(/WEBSITE/g, games[i].website)
+    .replace(/GITHUB/g, games[i].github);
+  entry = entry.replace('<a href=\'http:///\'></a>', '-');
+  content += entry;
 }
-const contentToCache = appShellFiles.concat(gamesImages);
+document.getElementById('content').innerHTML = content;
 
-// Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
+// Registering Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/pwa/js/sw.js');
+}
+
+// Requesting permission for Notifications after clicking on the button
+const button = document.getElementById('notifications');
+button.addEventListener('click', () => {
+  Notification.requestPermission().then((result) => {
+    if (result === 'granted') {
+      randomNotification();
+    }
+  });
 });
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
+// Setting up random Notification
+function randomNotification() {
+  const randomItem = Math.floor(Math.random() * games.length);
+  const notifTitle = games[randomItem].name;
+  const notifBody = `Created by ${games[randomItem].author}.`;
+  const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30000);
+}
+
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
